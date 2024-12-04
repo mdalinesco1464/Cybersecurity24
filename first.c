9522d8af5b8a178fbfdd0212cc6582e2bb7886af
mdalinesco1464 <mdalinesco1464@gmail.com>
2024-12-04 21:12:47 +0600

    Showing hello world

---
+++ first.c
@@ -0,0 +1,20 @@
+/*
+    Author: MD. Younus Ali
+    Date: 2024-12-04
+    Description:  Git commit how changes and impact  in c code
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * @brief Brief description of the main function
+ * 
+ * @return int - Describe the return value
+ */
+int main() {
+    // TODO: Implement functionality
+    printf("Hello, World!\n");
+
+    return 0;
+}
